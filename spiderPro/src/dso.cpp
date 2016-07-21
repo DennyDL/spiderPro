9f1c852f4046b82273707ff517e772c6c7b30a90
Denny <blue_maple_leaf@126.com>
2016-07-21 19:29:03 +0800

    2016 7 21 19:29

--- spiderPro/src/dso.cpp
+++ spiderPro/src/dso.cpp
@@ -1,31 +1,40 @@
 #include "dso.h"
-
-
+#include "qstring.h"
+#include <dlfcn.h>
 int DsoManager::load(char* path,char*name)
 {
 	Module *module  = new Module();
 	char * npath = strcat2(3, path, name, ".so");
-	
+
 	void* handle = dlopen(npath,RTLD_GLOBAL | RTLD_NOW);
 	if(handle == NULL)
 		return MODULE_ERR;
 
 	void *rv = (dlsym(handle,name));
 	if(rv == NULL)
-                return MODULE_ERR;
-	
+        return MODULE_ERR;
 
-	dlclose(handle);
+
+//	dlclose(handle);
 	module = (Module *)rv;//module结构体的入口
 //	module->init();//调用初始化函数
 //暂时不需要init（）;找到module入口地址就可以了
 
-	this->modules.insert(name,module);//插入管理的map中以便以后查询找到快速找到相应的模块入口地址
+	this->m_modules.insert(pair<string,Module*>(name,module));//插入管理的map中以便以后查询找到快速找到相应的模块入口地址
+
 	return MODULE_OK;
 
 }
 
 Module* DsoManager::getModule(char* name)
 {
-	return this->modules.find(name);
+	map<string, Module*>::iterator itor = this->m_modules.find(name);
+	if(itor != m_modules.end())
+	{
+		return itor->second;
+	}
+	else
+	{
+		return NULL;
+	}
 }
